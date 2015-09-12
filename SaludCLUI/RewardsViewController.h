b019318514d5dad513fdcaa80ba2839ae9fde461
vivek9088 <sridharvivek88@gmail.com>
2015-09-12 14:12:35 -0400

    created rewards header file

---
+++ SaludCLUI/RewardsViewController.h
@@ -0,0 +1,15 @@
+//
+//  RewardsViewController.h
+//  SaludCLUI
+//
+//  Created by Vivek Sridhar on 8/28/15.
+//  Copyright (c) 2015 Vanguards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RewardsViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *rewardsButton;
+
+@end
