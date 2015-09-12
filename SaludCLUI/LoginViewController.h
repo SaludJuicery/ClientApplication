7e18a0eee1a88405d9e43cb926bd8e17b4524953
vivek9088 <sridharvivek88@gmail.com>
2015-09-12 14:05:18 -0400

    Added Login header file

---
+++ SaludCLUI/LoginViewController.h
@@ -0,0 +1,26 @@
+//
+//  ViewController.h
+//  SaludCLUI
+//
+//  Created by Vivek Sridhar on 8/19/15.
+//  Copyright (c) 2015 Vanguards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PDKeychainBindings.h"
+
+@interface LoginViewController : UIViewController {
+    BOOL checked;
+    IBOutlet UIButton *checkBoxButton;
+    PDKeychainBindings *bindings;
+}
+- (IBAction)checkBoxButton:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *userLogin;
+
+@property (weak, nonatomic) IBOutlet UITextField *password;
+
+@property (weak, nonatomic) IBOutlet UITextField *username;
+
+@end
+
