0d05198236f3a0693710364ccf777ce10e610541
vivek9088 <sridharvivek88@gmail.com>
2015-09-12 14:03:17 -0400

    Adding Salud Delegate files

---
+++ SaludCLUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SaludCLUI
+//
+//  Created by Vivek Sridhar on 8/19/15.
+//  Copyright (c) 2015 Vanguards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
