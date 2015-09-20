5d97780e0874cabed33c86809d4f2d66c4184168
vivek9088 <sridharvivek88@gmail.com>
2015-09-20 18:32:27 -0400

    Added new string variable

--- SaludCLUI/CreateMenuController.h
+++ SaludCLUI/CreateMenuController.h
@@ -12,8 +12,15 @@
 
 - (IBAction)submitButton:(id)sender;
 - (IBAction)clearButton:(id)sender;
+- (void)displayMessage:(NSString *)msg;
 
 @property (weak, nonatomic) IBOutlet UIButton *submit;
 @property (weak, nonatomic) IBOutlet UIButton *clear;
+@property (weak, nonatomic) IBOutlet UITextField *itemCat;
+@property (weak, nonatomic) IBOutlet UITextField *itemName;
+@property (weak, nonatomic) IBOutlet UITextField *itemPrice;
+@property (weak, nonatomic) IBOutlet UITextView *itemDesc;
+@property (weak, nonatomic) IBOutlet UITextView *itemIngre;
+@property(weak,nonatomic) NSString *tempCat;
 
 @end
