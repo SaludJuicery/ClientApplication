a445f9b58f14048efd1032833610bc69b090d6b4
vivek9088 <sridharvivek88@gmail.com>
2015-10-11 17:03:15 -0400

    Made comments

--- SaludCLUI/BorderGlobalStyle.m
+++ SaludCLUI/BorderGlobalStyle.m
@@ -8,6 +8,21 @@
 
 #import "BorderGlobalStyle.h"
 
+
 @implementation BorderGlobalStyle
+
+- (void)setBorderForColor:(id *)color
+                    width:(float)width
+                   radius:(float)radius
+                    field:(id *)field
+                     text:(id *)txtfield
+{
+/*
+    self.layer.cornerRadius = radius;
+    self.layer.masksToBounds = YES;
+    self.layer.borderColor = [color CGColor];
+    self.layer.borderWidth = width;
+ */
+}
 
 @end
