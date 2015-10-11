128b80f4cff89bf452ed31a2b545d59b43593ab9
vivek9088 <sridharvivek88@gmail.com>
2015-10-11 17:04:10 -0400

    Added new fields

--- SaludCLUI/UpdateMenuController.h
+++ SaludCLUI/UpdateMenuController.h
@@ -8,6 +8,26 @@
 
 #import <UIKit/UIKit.h>
 
-@interface UpdateMenuController : UIViewController
+@interface UpdateMenuController : UIViewController <UITableViewDataSource,UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *itemCat;
+@property (weak, nonatomic) IBOutlet UITextField *itemName;
+@property (weak, nonatomic) IBOutlet UITextField *itemPrice;
+@property (weak, nonatomic) IBOutlet UITextView *itemDesc;
+@property (weak, nonatomic) IBOutlet UITextView *itemIngre;
+@property (weak, nonatomic) IBOutlet UIButton *update;
+@property (weak, nonatomic) IBOutlet UIButton *clear;
+@property (weak, nonatomic) IBOutlet UITextField *itemText;
+@property (strong,nonatomic) NSArray *autocompleteData1;
+@property (strong,nonatomic) NSArray *itemArray;
+@property (weak, nonatomic) IBOutlet UITableView *itemList;
+@property (weak, nonatomic) IBOutlet UITableView *catList;
+@property (weak, nonatomic) NSString *TempStr;
+@property (weak, nonatomic) IBOutlet UIButton *showItems;
+
+-(void)displayMsg:(NSString*)msg;
+- (IBAction)showItems:(id)sender;
+- (IBAction)UpdateMenu:(id)sender;
+- (IBAction)Clear:(id)sender;
 
 @end
