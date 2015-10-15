435189bdf3688a355ae5befd59d739893b4136c8
vivek9088 <sridharvivek88@gmail.com>
2015-10-14 20:30:36 -0400

    Removed display function

--- SaludCLUI/UpdateMenuViewController.h
+++ SaludCLUI/UpdateMenuViewController.h
@@ -10,4 +10,17 @@
 
 @interface UpdateMenuViewController : UIViewController
 
+
+@property (weak, nonatomic) IBOutlet UITextField *itemCat;
+@property (weak, nonatomic) IBOutlet UITextField *itemName;
+@property (weak, nonatomic) IBOutlet UITextField *itemPrice;
+@property (weak, nonatomic) IBOutlet UITextView *itemDesc;
+@property (weak, nonatomic) IBOutlet UITextView *itemIngre;
+@property (weak, nonatomic) IBOutlet UIButton *update;
+@property (weak, nonatomic) IBOutlet UIButton *clear;
+@property (weak,nonatomic) NSString *tempName;
+
+- (IBAction)UpdateMenu:(id)sender;
+- (IBAction)Clear:(id)sender;
+
 @end
