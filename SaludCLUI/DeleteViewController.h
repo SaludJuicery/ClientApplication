e9db55e24a6b40dfc7e4b3b552d1211887093f1e
vivek9088 <sridharvivek88@gmail.com>
2015-11-08 23:51:01 -0500

    Added downpicker property

--- SaludCLUI/DeleteViewController.h
+++ SaludCLUI/DeleteViewController.h
@@ -14,19 +14,15 @@
     NSMutableArray *autoCompleteData;
     NSMutableArray *selectedObjects;
     UITableView *autoCompleteView;
-    MessageController *alertMsg;
 }
 
 @property (weak, nonatomic) IBOutlet UIButton *btnDelete;
-
 @property (strong,nonatomic) DownPicker *downPicker;
 @property (weak, nonatomic) IBOutlet UITextField *textField;
-
 @property (nonatomic, retain) NSMutableArray *autoCompleteData;
 @property (nonatomic, retain) NSMutableArray *selectedObjects;
 @property (nonatomic, retain) IBOutlet UITableView *autoCompleteView;
 
-//Action to delete items from db
 - (IBAction)deleteItems:(id)sender;
 
 @end
