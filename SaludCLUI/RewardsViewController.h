32b88a6bd3424b507a7b03c31da7ea78f483d05a
vivek9088 <sridharvivek88@gmail.com>
2015-11-08 23:49:33 -0500

    Added checkbox button

--- SaludCLUI/RewardsViewController.h
+++ SaludCLUI/RewardsViewController.h
@@ -17,20 +17,23 @@
 @property (strong,nonatomic) DownPicker *downPickerCat;
 @property (strong,nonatomic) DownPicker *downPickerItem;
 @property (strong,nonatomic) DownPicker *downPickerLoc;
-
 @property (weak, nonatomic) IBOutlet UITextField *textCategory;
 @property (weak, nonatomic) IBOutlet UITextField *textLocation;
 @property (weak, nonatomic) IBOutlet UITextField *textItem;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *rewardsButton;
 @property (weak, nonatomic) IBOutlet UITextField *textAmount;
-@property (weak, nonatomic) IBOutlet UITextField *textDate;
+@property (weak, nonatomic) IBOutlet UITextField *startDate;
+@property (weak, nonatomic) IBOutlet UITextField *endDate;
+
 @property (weak, nonatomic) IBOutlet UIButton *radioPrice;
 @property (weak, nonatomic) IBOutlet UIButton *radioPercent;
+@property (weak, nonatomic) IBOutlet UIButton *startcheckbox;
+@property (weak, nonatomic) IBOutlet UIButton *endcheckbox;
 
 -(IBAction)onRadioBtn:(id)sender;
+-(IBAction)onCheckBoxBtn:(id)sender;
 
 - (IBAction)addReward:(id)sender;
-
 - (IBAction)clearFields:(id)sender;
 
 @end
