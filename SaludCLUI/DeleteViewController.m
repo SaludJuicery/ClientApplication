74974e8afce95ec4333c9210e5b628766773cbf1
vivek9088 <sridharvivek88@gmail.com>
2015-11-08 23:51:38 -0500

    Working Dropdown + multiple delete with confirmation

--- SaludCLUI/DeleteViewController.m
+++ SaludCLUI/DeleteViewController.m
@@ -8,9 +8,11 @@
 
 #import "DeleteViewController.h"
 #import "MessageController.h"
+#import "RemoteLogin.h"
 
 @interface DeleteViewController ()
-
+//Only required when using built-in delete button for tableview
+//@property (strong, nonatomic) NSIndexPath *indexPathToBeDeleted;
 @end
 
 @implementation DeleteViewController
@@ -19,13 +21,12 @@
 @synthesize selectedObjects;
 
 - (void)viewDidLoad {
-     [super viewDidLoad];
+    [super viewDidLoad];
     
-    //Object for calling alert messages
-    alertMsg = [[MessageController alloc] init];
-
+    // create the array of data
     NSMutableArray* bandArray = [[NSMutableArray alloc] init];
     
+    // add some sample data
     //Get URL request as json data and loop by adding data to bandArray
     [bandArray addObject:@"Smoothies"];
     [bandArray addObject:@"Juices & Refereshers"];
@@ -33,11 +34,11 @@
     [bandArray addObject:@"Hot Drinks"];
     [bandArray addObject:@"Cleanses"];
     [bandArray addObject:@"Medicinal"];
-
+    
     // bind yourTextField to DownPicker
     self.downPicker = [[DownPicker alloc] initWithTextField:self.textField withData:bandArray];
     
-    //Fill this array with menu items based on category selected
+    
     self.autoCompleteData = [[NSMutableArray alloc] initWithObjects:@"JUICES & REFRESHERS",@"SMOOTHIES",@"HEALTH SHOTS",@"HOT BEVERAGES",@"BOWLS", nil];
     
     /*
@@ -48,14 +49,14 @@
     autoCompleteView.scrollEnabled = YES;
     autoCompleteView.hidden = NO;
     [autoCompleteView setEditing:YES animated:YES];
-   
+    
     
     /* This line will only show tableview visible cells*/
     self.autoCompleteView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
     
     //Below line is to show the autocompleteview in the screen
     [self.view addSubview:autoCompleteView];
-   
+    
 }
 
 
@@ -110,29 +111,23 @@
     return cell;
 }
 
-//Below code is for displaying checkmark inside tableviewcell
-#pragma mark UITableViewDelegate methods
 
-- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
-    //return UITableViewCellEditingStyleDelete;
-    return 3;
-    
-}
+#pragma mark UITableViewDelegate methods
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
-   //NSString *object = [autoCompleteData objectAtIndex:indexPath.row]; //This assumes that your table has only one section and all cells are populated directly into that section from sourceArray.
-  //  UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    //NSString *object = [autoCompleteData objectAtIndex:indexPath.row]; //This assumes that your table has only one section and all cells are populated directly into that section from sourceArray.
+    //  UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     
-   // [autoCompleteView deselectRowAtIndexPath:[autoCompleteView indexPathForSelectedRow] animated:NO];
+    // [autoCompleteView deselectRowAtIndexPath:[autoCompleteView indexPathForSelectedRow] animated:NO];
     //UITableViewCell *cell = [autoCompleteView cellForRowAtIndexPath:indexPath];
     /*if (cell.accessoryType == UITableViewCellAccessoryNone) {
-        cell.accessoryType = UITableViewCellAccessoryCheckmark;
-        [selectedObjects addObject:object];
-    } else if (cell.accessoryType == UITableViewCellAccessoryCheckmark) {
-        cell.accessoryType = UITableViewCellAccessoryNone;
-        [selectedObjects removeObjectIdenticalTo:object];
-    }*/
+     cell.accessoryType = UITableViewCellAccessoryCheckmark;
+     [selectedObjects addObject:object];
+     } else if (cell.accessoryType == UITableViewCellAccessoryCheckmark) {
+     cell.accessoryType = UITableViewCellAccessoryNone;
+     [selectedObjects removeObjectIdenticalTo:object];
+     }*/
     
 }
 
@@ -150,69 +145,123 @@
 }
 
 /*
-#pragma mark - Navigation
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    //Below code is for deleting a row without confirmation
+    /*if (editingStyle == UITableViewCellEditingStyleDelete) {
+     NSInteger index = indexPath.row;
+     [autoCompleteData removeObjectAtIndex:index];
+     
+     [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+     withRowAnimation:UITableViewRowAnimationFade];
+     
+     }*/
+    
+    //Below code is for deleting a row with confirmation
+    /*
+     if (editingStyle == UITableViewCellEditingStyleDelete) {
+     
+     // self.indexPathToBeDeleted = indexPath;
+     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Warning"
+     message:@"Are you sure?"
+     delegate:self
+     cancelButtonTitle:@"NO"
+     otherButtonTitles:@"YES", nil];
+     [alert show];
+     
+     
+     }*/
+    
 }
-*/
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        
-        // Delete the row from the data source
-        
-        if (indexPath.section == 0){
-            [self.autoCompleteView beginUpdates];
-            NSPredicate *pred = [NSPredicate predicateWithFormat:@"Category =[cd] %@", @"Category 1"];
-            NSArray * catArray = [self.selectedObjects filteredArrayUsingPredicate:pred];
-            [self.selectedObjects removeObject:catArray[indexPath.row]];
-            [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObjects:indexPath, nil] withRowAnimation:NO];
-            [self.autoCompleteView endUpdates];
-        }
-        
-    }
-}
-
+/*
+ - (UITableViewCellEditingStyle)tableView:(UITableView *)aTableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ if (autoCompleteView.editing)
+ {
+ return UITableViewCellEditingStyleDelete;
+ }
+ 
+ return UITableViewCellEditingStyleNone;
+ }
+ */
+//Below code is for deleting multiple items at once
 - (IBAction)deleteItems:(id)sender {
     
-   /* NSArray *selectedCells = [self.autoCompleteView indexPathsForSelectedRows];
-    NSMutableIndexSet *indicesToDelete = [[NSMutableIndexSet alloc] init];
-    for (NSIndexPath *indexPath in selectedCells) {
-        [indicesToDelete addIndex:indexPath.row];
-    }
-    //arrayFromPlist is NSMutableArray
-    [autoCompleteView beginUpdates];
-    [autoCompleteView deleteRowsAtIndexPaths:selectedCells withRowAnimation:UITableViewRowAnimationAutomatic];
-    [autoCompleteView endUpdates];
-    [selectedObjects removeObjectsAtIndexes:indicesToDelete];
-    [autoCompleteView reloadData];
-    */
-    [alertMsg deleteConfirmation:@"Do you want to delete these items?"];
+    //Below commented code not working
+    // MessageController *alertMsg = [[MessageController alloc]init];
+    //[alertMsg deleteConfirmation:@"Do you want to delete these items?"];
+    
+    UIAlertView *deleteConfirm = [[UIAlertView alloc]
+                                  initWithTitle:@"Confirmation"
+                                  message:@"Are you sure to delete?"
+                                  delegate:self
+                                  cancelButtonTitle:@"NO"
+                                  otherButtonTitles:@"YES", nil];
+    
+    [deleteConfirm show];
 }
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
-    //Code for OK Button
-    if (buttonIndex == 0)
+    NSString *title = [alertView buttonTitleAtIndex:buttonIndex];
+    if([title isEqualToString:@"NO"])
     {
-        NSLog(@"Ok");
-        //NSArray* selectedRows = [autoCompleteView indexPathsForSelectedRows];
-        [autoCompleteView deleteRowsAtIndexPaths:selectedObjects
-                                withRowAnimation:UITableViewRowAnimationRight];
-        [autoCompleteData removeObjectsInArray:selectedObjects];
+        NSLog(@"Nothing to do here");
+    }
+    else if([title isEqualToString:@"YES"])
+    {
+        
+        NSArray *selectedCells = [self.autoCompleteView indexPathsForSelectedRows];
+        NSMutableIndexSet *indicesToDelete  = [[NSMutableIndexSet alloc] init];
+        for (NSIndexPath *indexPath in selectedCells) {
+            [indicesToDelete addIndex:indexPath.row];
+        }
+        
+        //Below code is for manual Delete Button
+        [autoCompleteData removeObjectsAtIndexes:indicesToDelete];
+        [autoCompleteView beginUpdates];
+        [autoCompleteView deleteRowsAtIndexPaths:selectedCells withRowAnimation:UITableViewRowAnimationAutomatic];
+        [autoCompleteView endUpdates];
         [autoCompleteView reloadData];
+        
+        MessageController *msg = [[MessageController alloc]init];
+        
+        NSArray *keys = [NSArray arrayWithObjects:@"items", nil];
+        NSArray *objects = [NSArray arrayWithObjects:autoCompleteData, nil];
+        
+        NSString *url = @"http://ec2-52-88-11-130.us-west-2.compute.amazonaws.com:3000/menu/reward/insert";
+        
+        RemoteLogin *remote = [[RemoteLogin alloc] init];
+        int res = [remote getConnection:keys forobjects:objects forurl:url];
+        
+        if(res==1)
+        {
+            [msg displayMessage:@"Connection Error: Please try again..."];
+        }
+        else
+        {
+            [msg displayMessage:@"Menu Items Deleted Successfully"];
+        }
+
+        
+        
+        /* Below code is for built-in delete button
+         [autoCompleteData removeObjectAtIndex:[self.indexPathToBeDeleted row]];
+         [autoCompleteView deleteRowsAtIndexPaths:[NSArray arrayWithObject:self.indexPathToBeDeleted] withRowAnimation:UITableViewRowAnimationFade];
+         [autoCompleteView reloadData];*/
     }
     
-    //Code for Cancel Button
-    if (buttonIndex == 1)
-    {
-         NSLog(@"Cancel");
-        
-    }
 }
 @end
 
